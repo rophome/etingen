2a447da841f02628ea36983a9595f4cf8e84a4a9
Ronnie <spamfilter@rophome.dk>
2020-03-12 14:49:25 +0100

    added styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='directory-menu'>
-        <div className='menu-item'>
-            <div className='content'>
-                <h1 className='title'>{title}</h1>
-                <span className='subtitle'>SHOP HOW</span>
-            </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-)
+);
 
 export default MenuItem;
